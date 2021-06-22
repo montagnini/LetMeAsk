0e5597532cc00af589e64f45b8966af81a4cf45e
montagnini <we.montagnini@gmail.com>
2021-06-22 02:16:38 -0300

    Adicionado página de criação de sala; Adicionado página de entrar em uma sala.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import "./styles/global.scss";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import { useHistory } from "react-router-dom";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleImg from "../assets/images/google-icon.svg";
+
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom(){
+      history.push('/rooms/new');
+  }
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Cria salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real!</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo (Let Me Ask)" />
+          <button  onClick={navigateToNewRoom} className="create-room">
+            <img src={googleImg} alt="Google" />
+            Cria sua sala com o Google.
+          </button>
+          <div className="separator"> ou entre em uma sala! </div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala." />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
